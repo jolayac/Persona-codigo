c82dc2880266f38d38bbe70f048a8252d97c35d2
jolayac <jolayac@unal.edu.co>
2025-09-18 16:10:30 -0500

    Avance 4pm

--- Contraseña.py
+++ Contraseña.py
@@ -1,5 +1,13 @@
-Fron Persona import Persona
-
-
 class Contraseña:
-    pass
+    def __init__(self, contraseña):
+        self.contraseña = contraseña
+
+    def crear_contraseña(self, contraseña):
+        while True:
+            print(
+                "Ingrese una contraseña.\nTenga en cuenta que la contraseña debe cumplir con lo siguinte:")
+            input(
+                "-Mínimo 8 caracteres.\n-Combinar mayúsclas y minúsculas.\nTener un caracter especial.")
+            if len(contraseña) < 8:
+                print(
+                    "La contraseña debe tener 8 caracteres como mínimo.\nIntente de nuevo.\n>")
--- Persona.py
+++ Persona.py
@@ -1,15 +1,21 @@
+from Contraseña import Contraseña
+
+
 class Persona (nombre, edad):
-    def 
+    def __init__(self, nombre, edad, identificacion):
+        self.nombre = nombre
+        self.edad = edad
+        self.identificacion = identificacion
 
-
+    def mostrar_info(self):
+        print(
+            f"| Nombre: {self.nombre}.\n| Edad: {edad}.\n| Nº de identificación: {self.identificacion}.")
 
 
 def main():
 
-
-
-
-
     pass
+
+
 if __name__ == "__main__":
     main()
